a356a3c02f1ca979ae40bd2e0b5bc633d1a20fed
kate <katyacher@inbox.ru>
2022-07-29 09:26:07 +0300

    check_format() changed, translate menu item

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include <fstream>
 #include <sstream>
 
-bool check_dots_count(std::string ip_str){
+bool check_dots_count(std::string data){
     int dot_count = 0;
-    for(int i = 0; i < ip_str.length(); i++){
-        if(ip_str[i] == '.') dot_count++;
+    for(int i = 0; i < data.length(); i++){
+        if(data[i] == '.') dot_count++;
     }
-    return (dot_count > 2)? false: true;
+    return dot_count == 2;
 }
 
 bool is_leap(int year){
@@ -25,24 +25,42 @@ std::string get_part_str(std::string str) {
     }
     return part;
 }
-// if (part[i] >= '0' && part[i] <= '9'))
 
-bool check_format(std::string date){
-    for(int i = 0; i < 2; i++){
-        if ((get_part_str(date).length() != 1)|| (get_part_str(date).length() !=2)){
+bool is_number(std::string part){
+    for(int i = 0; i < part.length(); i++){
+        if (part[i] >= '0' && part[i] <= '9'){
+            continue;
+        }else {
             return false;
-        } else{
-            date.erase(0,get_part_str(date).length()+1);
         }
     }
-    if(get_part_str(date).length() != 4){
+    return true;
+}
+bool check_format(std::string date){
+    for(int i = 0; i < 2; i++){
+        std::string part = get_part_str(date);
+        if ( !(part.length() == 2  && is_number(part))){
+            return false;
+        } else
+            date.erase(0,part.length()+1);
+    }
+    std::string last_part = get_part_str(date);
+    if(!(last_part.length() == 4 && is_number(last_part))){
         return false;
     }
+
     return true;
 }
 
 bool validate_date(std::string date) {
-    // parse the date to day, month and year
+    if(!check_dots_count(date)){
+        return false;
+    }
+
+    if (!check_format(date)){
+        return false;
+    }
+
     int day = std::stoi(date.substr(0, 2));
     int month = std::stoi(date.substr(3, 2));
     int year = std::stoi(date.substr(6, 4));;
@@ -62,6 +80,7 @@ bool validate_date(std::string date) {
     }
     return false;
 }
+
 void write_to_statement(std::string file_name){
     std::ofstream file;
     file.open(file_name, std::ios::app);
@@ -70,18 +89,19 @@ void write_to_statement(std::string file_name){
     double salary;
 
     if(file.is_open()){
-        std::cout << "Введите имя: ";
+        std::cout <<"Enter data " << std::endl;
+        std::cout << "Name: ";
         std::cin >> name;
-        std::cout << "Введите фамилию: ";
+        std::cout << "Surname: ";
         std::cin >> surname;
-        std::cout << "Введите дату: ";
+        std::cout << "Date in format DD.MM.YYYY: ";
         std::cin >> date;
-        while(!(check_dots_count(date) && validate_date(date))){
+        while(!validate_date(date)){
             std::cout << "Incorrect date." << std::endl;
-            std::cout << "Введите дату: ";
+            std::cout << "Enter date in format DD.MM.YYYY: ";
             std::cin >> date;
         }
-        std::cout << "Введите зарплату: ";
+        std::cout << "Salary: ";
         std::cin >> salary;
         std::stringstream  record;
         record << name << " " << surname << " " << date << " " << salary;
@@ -113,10 +133,10 @@ int main() {
     std::string file_name = "/home/kate/CLionProjects/20_1_write_to_statement/statement.txt";
 
     while(true){
-        std::cout << "Выберете пункт меню: " << std::endl;
-        std::cout << "1 - внести запись в ведомость" << std::endl;
-        std::cout << "2 - просмотреть содержимое ведомости" << std::endl;
-        std::cout << "3 - Выйти из программы" << std::endl;
+        std::cout << "Select menu item: " << std::endl;
+        std::cout << "1 - Make an entry in the statement" << std::endl;
+        std::cout << "2 - View the contents of the statement" << std::endl;
+        std::cout << "3 - Exit the program" << std::endl;
 
         int menu;
         std::cin >> menu;
@@ -131,7 +151,7 @@ int main() {
             case 3:
                 return 0;
             default:
-                std::cout << "Такого пункта меню нет" << std::endl;
+                std::cout << "There is no such menu item." << std::endl;
                 break;
         }
     }
